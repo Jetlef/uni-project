c704e3489ee86aa75239bf3dc5ebc3ecbda3532c
Luca Pears <gesucristo34@gmail.com>
2013-12-27 12:47:10 +0100

    switched to python3 (automatic Unicode handling) and utf-8
    plus, some new headers in the files

--- libuni.py
+++ libuni.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # libuni.py
 #
 # Support library for uni.py
--- uni.py
+++ uni.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# uni.py
+#
+# The [UNI]versal encoder. ;D
+#
+# (c) 2013-4, Luca J Pears <lucapiras66@gmail.com>
+
 import sys
 import os
 import libuni
@@ -8,7 +17,9 @@ try:
     inputformat = args[1]
     outputformat = args[2]
     for mediafile in libuni.filterFiles(inputformat):
-        print libuni.Encoder(mediafile,outputformat).encode()
+        original, converted = libuni.Encoder(mediafile,outputformat).encode()
+        print("from:\t{}\nto:\t{}".format(original,converted))
+        print()
 except IndexError:
-    print 'uni: usage: uni.py <input> <output>'
-    print 'see uni-project.md for more details'
+    print('uni: usage: uni.py <input> <output>')
+    print('see uni-project.md for more details')
